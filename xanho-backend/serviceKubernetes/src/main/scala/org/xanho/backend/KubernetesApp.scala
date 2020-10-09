7ca52ab33ed517eea2fcdfb5169cb1d8095e21d2
Sean Cheatham <seancheatham@gmail.com>
2020-10-08 21:06:54 -0400

    Start AkkaManagement and ClusterBootstrap

--- xanho-backend/serviceKubernetes/src/main/scala/org/xanho/backend/KubernetesApp.scala
+++ xanho-backend/serviceKubernetes/src/main/scala/org/xanho/backend/KubernetesApp.scala
@@ -1,5 +1,14 @@
 package org.xanho.backend
 
+import akka.management.cluster.bootstrap.ClusterBootstrap
+import akka.management.scaladsl.AkkaManagement
 import org.xanho.knowledgegraph.service.Service
 
-object KubernetesApp extends App with Service
+object KubernetesApp extends App with Service {
+  import system.executionContext
+
+  AkkaManagement(system).start()
+    .map(_ =>
+      ClusterBootstrap(system).start()
+    )
+}
