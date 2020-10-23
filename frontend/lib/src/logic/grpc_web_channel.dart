75cc8e27351ea24c1b475575a30657b5cbcfa57a
Sean Cheatham <seancheatham@gmail.com>
2020-10-23 19:44:09 -0400

    Fix gRPC channel

--- frontend/lib/src/logic/grpc_web_channel.dart
+++ frontend/lib/src/logic/grpc_web_channel.dart
@@ -2,6 +2,5 @@ import 'package:grpc/grpc_connection_interface.dart';
 import 'package:grpc/grpc_web.dart';
 
 ClientChannelBase channel = GrpcWebClientChannel.xhr(
-  // Uri.parse("https://backend.xanho.org:443"),
-  Uri.parse("http://localhost:8447"),
+  Uri.parse("https://backend.xanho.org:443"),
 );
